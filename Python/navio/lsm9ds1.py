6cd1aa1cd9b7f15e1ac182d1bf18a442345065b7
Ozzy Simpson <14060578+ozzy-simpson@users.noreply.github.com>
2025-03-19 13:33:26 -0400

    LSM9DS1.c: replace xrange() with range() for Python 3 support
    This update replaces `xrange()` with `range()` to ensure compatibility with Python 3. Since `xrange()` was removed in Python 3, this change prevents runtime errors and aligns the codebase with modern Python versions.

--- Python/navio/lsm9ds1.py
+++ Python/navio/lsm9ds1.py
@@ -290,7 +290,7 @@ class LSM9DS1:
     def read_acc(self):
         # Read accelerometer
         response = self.readRegs(self.__DEVICE_ACC_GYRO, self.__LSM9DS1XG_OUT_X_L_XL, 6)
-        for i in xrange(3):
+        for i in range(0,3):
             self.accelerometer_data[i] = self.G_SI * (self.byte_to_float_le(response[2*i:2*i+2]) * self.acc_scale)
 
         self.accelerometer_data = [-self.accelerometer_data[1], -self.accelerometer_data[0], self.accelerometer_data[2]]
@@ -298,7 +298,7 @@ class LSM9DS1:
     def read_gyro(self):
         # Read gyroscope
         response = self.readRegs(self.__DEVICE_ACC_GYRO, self.__LSM9DS1XG_OUT_X_L_G, 6)
-        for i in xrange(3):
+        for i in range(0,3):
             self.gyroscope_data[i] = (self.PI / 180.0) * (self.byte_to_float_le(response[2*i:2*i+2]) * self.gyro_scale) 
 
         self.gyroscope_data = [-self.gyroscope_data[1], -self.gyroscope_data[0], self.gyroscope_data[2]]
@@ -306,7 +306,7 @@ class LSM9DS1:
     def read_mag(self):
         # Read magnetometer
         response = self.readRegs(self.__DEVICE_MAGNETOMETER, self.__LSM9DS1M_OUT_X_L_M, 6)
-        for i in xrange(3):
+        for i in range(0,3):
             self.magnetometer_data[i] = 100.0 * (self.byte_to_float_le(response[2*i:2*i+2]) * self.mag_scale)
 
         self.magnetometer_data[1] *=-1
@@ -324,17 +324,17 @@ class LSM9DS1:
 
         # Read accelerometer
         response = self.readRegs(self.__DEVICE_ACC_GYRO, self.__LSM9DS1XG_OUT_X_L_XL, 6)
-        for i in xrange(3):
+        for i in range(0,3):
             self.accelerometer_data[i] = self.G_SI * (self.byte_to_float_le(response[2*i:2*i+2]) * self.acc_scale)
 
         # Read gyroscope
         response = self.readRegs(self.__DEVICE_ACC_GYRO, self.__LSM9DS1XG_OUT_X_L_G, 6)
-        for i in xrange(3):
+        for i in range(0,3):
             self.gyroscope_data[i] = (self.PI / 180.0) * (self.byte_to_float_le(response[2*i:2*i+2]) * self.gyro_scale) 
 
         # Read magnetometer
         response = self.readRegs(self.__DEVICE_MAGNETOMETER, self.__LSM9DS1M_OUT_X_L_M, 6)
-        for i in xrange(3):
+        for i in range(0,3):
             self.magnetometer_data[i] = 100.0 * (self.byte_to_float_le(response[2*i:2*i+2]) * self.mag_scale)
 
         # Change rotation of LSM9DS1 like in MPU-9250
